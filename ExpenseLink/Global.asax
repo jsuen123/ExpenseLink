5810b1fb40bd5d39415d820f5bb3eb6bd67c7b54
John Suen <johnnie.suen@gmail.com>
2017-10-08 20:11:11 +1100

    Add project files.

---
+++ ExpenseLink/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExpenseLink.MvcApplication" Language="C#" %>
---
+++ ExpenseLink/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExpenseLink.Startup))]
+namespace ExpenseLink
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
